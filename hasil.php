759e1c279dc15b619f9a4c27b9d8b4c5fcbd2051
Ganteng28 <abdulfarisdin@gmail.com>
2024-06-19 19:21:16 +0700

    Add files via upload

---
+++ hasil.php
@@ -0,0 +1,7 @@
+<?php
+echo "<center>nama: ".$_POST['nama']."</center><br>";
+echo "<center>Username: ".$_POST['username']."</center><br>";
+echo "<center>email: ".$_POST['email']."</center><br>";
+echo "<center>tanggal: ".$_POST['tanggal']."</center><br>";
+echo "<center>password: ".$_POST['password']."</center><br>";
+?>
